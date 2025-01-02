7aef45362a0299e5721e87b47ea61446f11a8641
Manu <manohar142652@gmail.com>
2025-01-02 17:42:43 +0530

    Updates done

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Change this import
+
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root')); // Use createRoot
+root.render(<App />); // Render your app using the new API
